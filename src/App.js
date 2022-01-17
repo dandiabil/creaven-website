40d2e9e0a00c15e4adeb980ae84c9b784fcbdd8e
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2022-01-17 23:11:44 +0700

    commit #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primereact/resources/primereact.min.css";
+import "primeflex/primeflex.css";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
+import ConsultPage from "./pages/ConsultPage/ConsultPage";
+import SurveyPage from "./pages/SurveyPage/SurveyPage";
+import PostSurveyPage from "./pages/SurveyPage/PostSurveyPage/PostSurveyPage";
+import ConsultResultPage from "./pages/ConsultPage/ConsultResultPage/ConsultResultPage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<Layout element={<LandingPage />} />}
+          />
+          <Route path="/login" element={<Layout element={<LoginPage />} />} />
+          <Route
+            path="/register"
+            element={<Layout element={<RegisterPage />} />}
+          />
+          <Route
+            path="/contact"
+            element={<Layout element={<ContactPage />} />}
+          />
+          <Route
+            path="/consult"
+            element={<Layout element={<ConsultPage />} />}
+          />
+          <Route
+            path="/consult/results"
+            element={<Layout element={<ConsultResultPage />} />}
+          />
+          <Route path="/survey" element={<Layout element={<SurveyPage />} />} />
+          <Route
+            path="/survey/success"
+            element={<Layout element={<PostSurveyPage />} />}
+          />
+          <Route
+            path="/profile"
+            element={<Layout element={<ProfilePage />} />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
